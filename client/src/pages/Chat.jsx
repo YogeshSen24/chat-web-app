e670b69602db48dfe212cc1cfeeeefe5a42b1124
YogeshSen24 <yose0026@gmail.com>
2024-03-09 16:01:38 +0530

    fixing real time message essue

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -8,19 +8,33 @@ import { SocketContext } from "../context/SocketContextProvider.jsx";
 
 function Chat() {
   const { receiver } = useParams();
+  const [receiverData , setReceiverData] = useState(null)
   const [conversation, setConversation] = useState([]);
   const [message, setMessage] = useState("");
-  var user = localStorage.getItem("user");
+  var userId = localStorage.getItem("user");
   const { socket } = useContext(SocketContext);
-
+  const fetchConversation = async () => {
+    try {
+      const res = await axios.post(
+        `http://localhost:8000/api/message/${receiver}`,
+        {
+          sender: userId,
+        }
+      );
+      setConversation(res.data.messages);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   const sendMessage = async () => {
     try {
       await axios.post(`http://localhost:8000/api/message/send/${receiver}`, {
         message,
-        sender: user,
+        sender: userId,
       });
-      socket.emit("message", message);
+socket.emit("message", message);
       setMessage("");
+      fetchConversation(userId,receiver)
     } catch (error) {
       console.log(error);
     }
@@ -35,42 +49,43 @@ function Chat() {
   };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.post(
-          `http://localhost:8000/api/message/${receiver}`,
-          {
-            sender: user,
-          }
-        );
-        setConversation(res.data.messages);
-      } catch (error) {
-        console.log(error);
-      }
-    };
 
-    fetchData();
-
+   
+    fetchConversation();
     socket?.on(
       "newMessage",
       (newMessage) => {
+        // console.log(newMessage)
         setConversation([...conversation, newMessage]);
         return () => socket?.off(newMessage);
       },
       [socket, setConversation, conversation]
     );
-  });
+
+    const fetchReceiverData = async()=>{
+      try {
+        let res = await axios.get(`http://localhost:8000/api/users/${receiver}`)
+        setReceiverData(res.data)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchReceiverData()
+  },[]);
   return (
     <div className="shadow-2xl  w-full p-5 m-5 ">
+      <div className="w-screen p-5 bg-blue-500">
+        <h2 className="text-3xl font-bold">{receiverData&&receiverData[0]?.name}</h2>
+      </div>
       <div className=" w-full h-5/6 mb-10 overflow-y-scroll">
         {conversation?.map((item) => (
           <div
             key={item._id}
-            className={`chat chat-${
-              receiver === item.receiverId ? "start" : "end"
+            className={`flex my-4 ${
+              receiver === item.receiverId ? "justify-end" : "justify-start"
             }`}
           >
-            <div className="chat-bubble chat-bubble-primary">
+            <div className="p-2 bg-blue-600 rounded-3xl">
               {item.message}
             </div>
           </div>
