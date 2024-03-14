1483c1138c37b15cb2b97efabf4339ef042e375b
YogeshSen24 <yose0026@gmail.com>
2024-03-14 20:14:22 +0530

    pre deployment checks

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import dotenv from "dotenv"
 import authRoute from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
@@ -8,6 +9,7 @@ import cors from "cors"
 import {app , server}  from "./socket.js"
 import express from "express"
 
+const __dirname = path.resolve()
 dotenv.config()
 app.use(cors({
   origin: "*",
@@ -18,6 +20,12 @@ app.use(cookieParser())
 app.use("/api/auth" , authRoute)
 app.use("/api/message" , messageRoute)
 app.use("/api/users" , userRoute)
+
+app.use(express.static(path.join(__dirname,"/client/dist")))
+
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"client","dist","index.html"))
+})
 
 server.listen(process.env.PORT||8000 , ()=>{
     console.log("server is running on port :: " , process.env.PORT||8000);
